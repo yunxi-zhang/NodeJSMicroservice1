73c29bb6127bd4d8e8a24ca85cc4f4b74b8d1880
yunxi.zhang <yunxi.zhang@accenture.com>
2020-12-29 23:03:56 +0000

    change function syntax to lambda

--- supplier.js
+++ supplier.js
@@ -2,38 +2,38 @@ module.exports = function supplier(options) {
     /**
      * Get a list of all suppliers
      */
-    this.add('role:supplier,cmd:list', function (msg, reply) {
-      this.make('supplier').list$({}, function (err, supplier) {
-        if (err) {
-          reply(err);
-        } else {
-          reply(null, supplier)
-        }
-      })
+    this.add('role:supplier,cmd:list', (msg, reply) => {
+        this.make('supplier').list$({}, (err, supplier) => {
+            if (err) {
+                reply(err);
+            } else {
+                reply(null, supplier)
+            }
+        })
     })
-      /**
-     * Get a list of filtered suppliers by type
-     */
-    this.add('role:supplier,cmd:list,criteria:byType', function (msg, reply) {
-      this.make('supplier').list$({ type: msg.args.params.type }, function (err, supplier) {
-        if (err) {
-          reply(err);
-        } else {
-          reply(null, supplier)
-        }
-      })
+    /**
+   * Get a list of filtered suppliers by type
+   */
+    this.add('role:supplier,cmd:list,criteria:byType', (msg, reply) => {
+        this.make('supplier').list$({ type: msg.args.params.type }, (err, supplier) => {
+            if (err) {
+                reply(err);
+            } else {
+                reply(null, supplier)
+            }
+        })
     })
     /**
      * Load a supplier by its Id
      */
-    this.add('role:supplier,cmd:load', function (msg, reply) {
-      this.make('supplier').load$(msg.args.params.id, function (err, supplier) {
-        if (err) {
-          reply(err);
-        } else {
-          reply(null, supplier)
-        }
-      })
+    this.add('role:supplier,cmd:load', (msg, reply) => {
+        this.make('supplier').load$(msg.args.params.id, (err, supplier) => {
+            if (err) {
+                reply(err);
+            } else {
+                reply(null, supplier)
+            }
+        })
     })
     /**
      * Add a new supplier
@@ -42,15 +42,15 @@ module.exports = function supplier(options) {
      *  "balance": "10"
      * }
      */
-    this.add('role:supplier,cmd:create', function (msg, reply) {
-      var supplier = this.make('supplier')
-      supplier.balance = JSON.parse(msg.args.body).balance
-      supplier.save$(function (err, saved_supplier) {
-        if (err) {
-          reply(err);
-        } else {
-          reply(null, saved_supplier)
-        }
-      })
+    this.add('role:supplier,cmd:create', (msg, reply) => {
+        var supplier = this.make('supplier')
+        supplier.balance = JSON.parse(msg.args.body).balance
+        supplier.save$(function (err, saved_supplier) {
+            if (err) {
+                reply(err);
+            } else {
+                reply(null, saved_supplier)
+            }
+        })
     })
 }
